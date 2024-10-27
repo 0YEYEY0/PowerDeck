664acd97fad362ecaf195ceffd18a7e0a651f7fe
xDShiroxD <106044370+xDShiroxD@users.noreply.github.com>
2024-10-27 13:44:15 -0600

    [FIX] Loggin

--- Loggin.py
+++ Loggin.py
@@ -1,10 +1,6 @@
 import json
 import random
 import hashlib
-<<<<<<< HEAD
-=======
-import Album
->>>>>>> origin/Crear_Cuenta
 import tkinter as tk
 from tkinter import messagebox
 
@@ -23,22 +19,15 @@ def asignar_cartas(cartas_disponibles, cantidad=20):
 
 
 # Crea una cuenta de usuario
-<<<<<<< HEAD
 def crear_cuenta(nombre_usuario, contraseña, correo, nombre_persona, pais, es_administrador, cartas_disponibles):
-=======
-def crear_cuenta(nombre_usuario, contraseña, es_administrador, cartas_disponibles):
->>>>>>> origin/Crear_Cuenta
     hash_contraseña = hashlib.sha256(contraseña.encode()).hexdigest()  # Cifra la contraseña
 
     cuenta = {
         "nombre_usuario": nombre_usuario,
         "contraseña": hash_contraseña,
-<<<<<<< HEAD
         "correo": correo,
         "nombre_persona": nombre_persona,
         "pais": pais,
-=======
->>>>>>> origin/Crear_Cuenta
         "es_administrador": es_administrador,
         "cartas": []
     }
@@ -53,7 +42,6 @@ def guardar_cuenta(cuenta, ruta_archivo):
         json.dump(cuenta, archivo, indent=4)
 
 
-<<<<<<< HEAD
 # Valida que se haya ingresado todos los datos
 def validacion_de_datos(nombre_usuario, contraseña, correo, nombre_persona, pais):
     if not nombre_usuario or not contraseña or not correo or not nombre_persona or not pais:
@@ -76,46 +64,23 @@ def procesar_creacion_cuenta():
         return  # Detener si la validación falla
 
     # Carga las cartas en cartas_disponible desde el archivo json
-=======
-# Procesa la creación de cuenta desde la interfaz
-def procesar_creacion_cuenta():
-    nombre_usuario = entry_nombre.get()
-    contraseña = entry_contraseña.get()
-    es_administrador = var_tipo.get() == "Administrador"
-
-    # Validacioón de datos
-    if not nombre_usuario or not contraseña:
-        messagebox.showerror("Error", "Por favor, ingresa un nombre de usuario y una contraseña.")
-        return
-
-    # Carga las cartas en cartas_disponible desde el archuvo json
->>>>>>> origin/Crear_Cuenta
     try:
         cartas_disponibles = cargar_cartas("cartas.json")
     except FileNotFoundError:
         messagebox.showerror("Error", "Archivo de cartas no encontrado.")
         return
 
-<<<<<<< HEAD
     cuenta = crear_cuenta(nombre_usuario, contraseña, correo, nombre_persona, pais, es_administrador, cartas_disponibles)
-=======
-    cuenta = crear_cuenta(nombre_usuario, contraseña, es_administrador, cartas_disponibles)
->>>>>>> origin/Crear_Cuenta
     ruta_guardado = f"{nombre_usuario}_cuenta.json"
     guardar_cuenta(cuenta, ruta_guardado)
     messagebox.showinfo("Cuenta Creada", "Cuenta creada con éxito.")
 
-<<<<<<< HEAD
     # Vaciar todos los campos después de crear la cuenta
     entry_usuario.delete(0, tk.END)
     entry_contraseña.delete(0, tk.END)
     entry_correo.delete(0, tk.END)
     entry_nombre.delete(0, tk.END)
     entry_pais.delete(0, tk.END)
-=======
-    entry_nombre.delete(0, tk.END)
-    entry_contraseña.delete(0, tk.END)
->>>>>>> origin/Crear_Cuenta
 
 
 # Abre la ventana del administrador
@@ -125,11 +90,7 @@ def abrir_ventana_administrador():
     ventana_admin.geometry("300x150")
 
     # Botón para ver álbum
-<<<<<<< HEAD
     boton_ver_album = tk.Button(ventana_admin, text="Ver Álbum", command=lambda: None)
-=======
-    boton_ver_album = tk.Button(ventana_admin, text="Ver Álbum", command= Album.main)
->>>>>>> origin/Crear_Cuenta
     boton_ver_album.pack(pady=10)
 
     # Botón para crear carta
@@ -147,7 +108,6 @@ def abrir_ventana_jugador(cuenta):
     boton_ver_album = tk.Button(ventana_jugador, text="Ver Álbum", command=lambda: None)
     boton_ver_album.pack(pady=10)
 
-<<<<<<< HEAD
     # Mostrar las cartas asignadas al jugador (opcional)
     messagebox.showinfo("Cartas Asignadas", f"Cartas asignadas:\n\n{cuenta['cartas']}")
 
@@ -160,19 +120,6 @@ def procesar_inicio_sesion():
     # Validación de datos
     if not validacion_de_datos(nombre_usuario, contraseña, None, None, None):
         return  # Detener si la validación falla
-=======
-
-
-# Verifica el nombre de usuario y contraseña para inciar sesión
-def procesar_inicio_sesion():
-    nombre_usuario = entry_nombre.get()
-    contraseña = entry_contraseña.get()
-
-    # Valida que se haya ingresado el nombre y la contraseña
-    if not nombre_usuario or not contraseña:
-        messagebox.showerror("Error", "Por favor, ingresa un nombre de usuario y una contraseña.")
-        return
->>>>>>> origin/Crear_Cuenta
 
     ruta_cuenta = f"{nombre_usuario}_cuenta.json"
     try:
@@ -197,7 +144,6 @@ def procesar_inicio_sesion():
 # Interfaz
 ventana = tk.Tk()
 ventana.title("Sistema de Cuenta")
-<<<<<<< HEAD
 ventana.geometry("300x450")
 
 # Nombre de usuario
@@ -205,15 +151,6 @@ label_usuario = tk.Label(ventana, text="Usuario:")
 label_usuario.pack(pady=5)
 entry_usuario = tk.Entry(ventana)
 entry_usuario.pack(pady=5)
-=======
-ventana.geometry("300x300")
-
-# Nombre de usuario
-label_nombre = tk.Label(ventana, text="Nombre de Usuario:")
-label_nombre.pack(pady=5)
-entry_nombre = tk.Entry(ventana)
-entry_nombre.pack(pady=5)
->>>>>>> origin/Crear_Cuenta
 
 # Contraseña
 label_contraseña = tk.Label(ventana, text="Contraseña:")
@@ -221,7 +158,6 @@ label_contraseña.pack(pady=5)
 entry_contraseña = tk.Entry(ventana, show="*")
 entry_contraseña.pack(pady=5)
 
-<<<<<<< HEAD
 # Nombre de la persona
 label_nombre = tk.Label(ventana, text="Nombre de la persona:")
 label_nombre.pack(pady=5)
@@ -240,8 +176,6 @@ label_pais.pack(pady=5)
 entry_pais = tk.Entry(ventana)
 entry_pais.pack(pady=5)
 
-=======
->>>>>>> origin/Crear_Cuenta
 # Check jugador-admin
 var_tipo = tk.StringVar(value="Jugador")
 label_tipo = tk.Label(ventana, text="Tipo de Cuenta:")
