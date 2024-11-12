d58ec4fa269221bf4587dcedaf3dd089765cf436
ShadowGlimmer <>
2024-11-11 22:22:27 -0600

    Se agregó más datos para los reportes

--- ventana_Reportes.py
+++ ventana_Reportes.py
@@ -7,16 +7,26 @@ def ver():
     # Interfaz
     ventana_reportes = tk.Tk()
     ventana_reportes.title("reportes")
-    ventana_reportes.geometry("300x200")
+    ventana_reportes.geometry("500x500")
 
     # Cantidad de jugadores registrados
-    label_info = tk.Label(ventana_reportes, text="jugadores registrados: " + str(reportes.jugadores_registrados()))
-    label_info.pack()
+    label_total_registros = tk.Label(ventana_reportes, 
+                                    text="Jugadores registrados: " + str(reportes.jugadores_registrados()), font=("bold"))
+    label_total_registros.pack(side = tk.TOP)
 
+    # Cantidad de veces las cartas fueron usadas
+    label_titulo_uso_cartas = tk.Label(ventana_reportes, text="Uso de cartas:", font=("bold"))
+    label_titulo_uso_cartas.pack(side = tk.TOP)
 
-    # info
-    label_info = tk.Label(ventana_reportes, text="info:")
-    label_info.pack(pady=5)
-    info_data = tk.Label(ventana_reportes, text=str(fileReader.cardAttribute("nombre", fileReader.read())))
-    info_data.pack(pady=5)
+    cardsTotal = len([element for element in list(fileReader.read()) if isinstance(element, dict)])
 
+    for i in range(cardsTotal):
+        if (fileReader.cardAttribute("es_principal", fileReader.read())[i]):
+            label_usos_cartas = tk.Label(ventana_reportes, 
+                                        text= str(fileReader.cardAttribute("nombre", fileReader.read())[i]) + ": " + str(i))
+            label_usos_cartas.pack(side = tk.TOP)
+        else:
+            label_usos_cartas = tk.Label(ventana_reportes,
+                                        text= str(fileReader.cardAttribute("nombre", fileReader.read())[i]) + "(variante)" ": " + str(i))
+            label_usos_cartas.pack(side = tk.TOP)
+ 
